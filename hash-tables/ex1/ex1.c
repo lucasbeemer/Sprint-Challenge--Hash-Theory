bd31f3861172161d70b349d04078333c74d52fa8
Lucas Beemer <lucasbeemer@gmail.com>
2019-01-18 08:58:07 -0800

    ex1, all tests passed

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -6,10 +6,30 @@
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
+  Answer *ret = malloc(sizeof(Answer));
 
-  // YOUR CODE HERE
+  if(length < 2) {
+    return NULL;
+  }
 
-  return NULL;
+  for(int i = 0; i < length; i++) {
+    hash_table_insert(ht, weights[i], i);
+  }
+
+  for(int y = 0; y < length; y++) {
+    int matching = hash_table_retrieve(ht, limit - weights[y]);
+    if(matching != -1) {
+      if(y > matching) {
+        ret->index_1 = y;
+        ret->index_2 = matching;
+      } else {
+        ret->index_1 = matching;
+        ret->index_2 = y;
+      }
+      destroy_hash_table(ht);
+      return ret;
+    }
+  }
 }
 
 void print_answer(Answer *answer)
